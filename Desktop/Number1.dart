7e7be6767e17406041057f01af50f7058ec1121b
Tshepo-14 <51974901+Tshepo-14@users.noreply.github.com>
2022-05-25 19:56:01 +0200

    first commit of app of the year dart

---
+++ Desktop/Number1.dart
@@ -0,0 +1,8 @@
+void main() {
+  String name='Tshepo';
+  String app='Instagram';
+  String fCity='Maldives';
+  
+  print('My name is $name my favourite app is $app and i want to go to $fCity');
+  
+}

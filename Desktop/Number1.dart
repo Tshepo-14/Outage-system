04a27c2d064fd13ac8eed3aa875f702d48fbe964
Tshepo-14 <51974901+Tshepo-14@users.noreply.github.com>
2022-05-26 15:37:14 +0200

    Delete Desktop directory

--- Desktop/Number1.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  String name='Tshepo';
-  String app='Instagram';
-  String fCity='Maldives';
-  
-  print('My name is $name my favourite app is $app and i want to go to $fCity');
-  
-}

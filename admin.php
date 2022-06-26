25719c7781db1faca1232ae208923d064972d6ac
Tshepo-14 <51974901+Tshepo-14@users.noreply.github.com>
2022-06-26 16:05:06 +0200

    Add files via upload

--- admin.php
+++ admin.php
@@ -4,6 +4,8 @@
 ?>
 
 
+
+
 <html>
 <head>
     <title> Outage system</title>
@@ -12,6 +14,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
    
    
 </head>
@@ -20,12 +25,19 @@
     <form name="f1" action = "authentication.php" method = "POST"> 
     <div class="container p-5 my-5 bg-dark text-white" style="text-align:center; border-radius: 25px;">
             <h1>        Outage System       </h1>
-                <label> UserName: </label>
-                <input type="text" id="user" name="user" required><br><br>
-                <label> Password : </label>
-                <input type="password" id="pass" name="pass" required><br><br>
-                <input type =  "submit">  
                 <br>
+                <div class="input-group" style="width: 50%; align-items: center; align-content: center; margin: auto;">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                    <input id="email" type="text" class="form-control" name="email" placeholder="Email">
+                </div>
+                <br>
+                <div class="input-group" style="width: 50%; align-items: center; align-content: center; margin: auto;">
+                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                    <input id="password" type="password" class="form-control" name="password" placeholder="Password">
+                </div>
+                <br>
+                <input type =  "submit" class="btn-primary">  
+                <br><br>
                 <a href="index.html"> User </a>
         </div>
     </form>

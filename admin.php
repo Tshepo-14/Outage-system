cc0b6813f0b76d5ebece53dab238a4fd24376fa0
Tshepo-14 <51974901+Tshepo-14@users.noreply.github.com>
2022-06-26 16:06:20 +0200

    Add files via upload

--- admin.php
+++ admin.php
@@ -28,12 +28,12 @@
                 <br>
                 <div class="input-group" style="width: 50%; align-items: center; align-content: center; margin: auto;">
                     <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
-                    <input id="email" type="text" class="form-control" name="email" placeholder="Email">
+                    <input id="user" type="text" class="form-control" name="email" placeholder="Email">
                 </div>
                 <br>
                 <div class="input-group" style="width: 50%; align-items: center; align-content: center; margin: auto;">
                     <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                    <input id="password" type="password" class="form-control" name="password" placeholder="Password">
+                    <input id="pass" type="password" class="form-control" name="password" placeholder="Password">
                 </div>
                 <br>
                 <input type =  "submit" class="btn-primary">  
